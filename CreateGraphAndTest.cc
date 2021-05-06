9b597061ca90c83b8244bcde949ff13948270fcd
Arnoldo Solis <arnoldosolis817@gmail.com>
2021-05-06 11:37:59 -0400

    Finished Question 1 Implementation

--- CreateGraphAndTest.cc
+++ CreateGraphAndTest.cc
@@ -1,32 +1,135 @@
 // CSCI 335 Homework #6
 
-// Your name: 
+// Your name: Arnoldo Solis
 
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
 
 #include "graph.h"
 
 using namespace std;
 
-int testGraphDriver(int argc, char **argv) {
-    
-    //Begin your code here. Feel free to add any helper functions or classes you need,
-    //as long as we only have to call this function to run the specified assignment.
-    
-    return 0;
+int testGraphDriver(int argc, char **argv)
+{
+  const string graph_filename(argv[1]);
+  const string query_filename(argv[2]);
+  //Begin your code here. Feel free to add any helper functions or classes you need,
+  //as long as we only have to call this function to run the specified assignment.
+
+  // Initialize ifstream object
+  std::ifstream graphTxt;
+  // Open file
+  graphTxt.open(graph_filename);
+  // Checks if file is able to open or if it existss
+  if (graphTxt.fail())
+  {
+    // Prints error message
+    std::cout << "Unable to open file or file does not exist!" << std::endl;
+    // Exits
+    exit(1);
+  }
+
+  // Will be used to read in graph file
+  std::string graph_input;
+  // stores amount of vertices that is retrieved from first line of text file
+  std::string amountOfV;
+  // Gets the string number of vertices in graph
+  getline(graphTxt, amountOfV);
+  // Converter
+  std::stringstream amountOfVInt(amountOfV);
+  // This will be where the amount of vertices will be stored
+  int v = 0;
+  // Converts the string number to int and inserts into int v
+  amountOfVInt >> v;
+  // Prints amount of vertices in graph
+  std::cout << "There are " << v << " vertices in the graph" << std::endl;
+
+  // Initialize graph object
+  Graph directedGraph(v);
+
+  while (getline(graphTxt, graph_input))
+  {
+    // Will be used to traverse source vertex
+    int vertexIterator = 1;
+    //
+    int vertex = 0;
+    int connectedVertex = 0;
+    double weight = 0.0;
+    std::stringstream graphinput(graph_input);
+    // std::cout << graph_input << std::endl;
+    // While string stream object is being read to
+    while (graphinput)
+    {
+      // Iterates from first vector that is being read (this happens after the amount of vertices that will be in graph has been read in)
+      if (vertexIterator == 1)
+      {
+        graphinput >> vertex;
+        //std::cout << "vertex: " << vertex << std::endl;
+      }
+      // Gets connected vertex from graphinput and inserts into connectedVertex
+      graphinput >> connectedVertex;
+      //std::cout << "connected vertex:" << connectedVertex << std::endl;
+      // Gets weight of connected vertex from graph input and inserts into weight
+      graphinput >> weight;
+      //std::cout << "weight of connected vertex: " << weight << std::endl;
+      // Gets connected vertex and its weight and adds it to current vertex adjacent list
+      if (graphinput)
+      {
+        directedGraph.addEdge(vertex, connectedVertex, weight);
+      }
+
+      // goes to next vertex
+      vertexIterator++;
+    }
+  }
+  graphTxt.close();
+  std::cout << "Graph was read" << std::endl;
+
+  // The below variables will be used to check if they are connected to each other
+  // currentVertex will be put through a function to check if destinationVertex is in its adjacency list
+  // if it is they are connected
+  int currentVertex = 0;
+  int destinationVertex = 0;
+
+  // Initialize ifstream object
+  std::ifstream adjQueriesTxt;
+  adjQueriesTxt.open(query_filename);
+  // Checks if file is able to open or if it existss
+  if (adjQueriesTxt.fail())
+  {
+    // Prints error message
+    std::cout << "Unable to open file or file does not exist!" << std::endl;
+    // Exits
+    exit(1);
+  }
+
+  std::string line;
+  while (adjQueriesTxt >> currentVertex >> destinationVertex)
+  {
+    // adjQueriesTxt >> currentVertex;
+    // adjQueriesTxt >> destinationVertex;
+    // std::cout << currentVertex << " " << destinationVertex << std::endl;
+    directedGraph.isConnected(currentVertex, destinationVertex);
+  }
+
+  return 0;
 }
 
 // Do not modify anything below this line
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-		cout << "Usage: " << argv[0] << " <GRAPH_File>" << "<ADJACENCY_QUERYFILE>" << endl;
-		return 0;
-    }
-
-    testGraphDriver(argc, argv);
-
+int main(int argc, char **argv)
+{
+  if (argc != 3)
+  {
+    cout << "Usage: " << argv[0] << " <GRAPH_File>"
+         << "<ADJACENCY_QUERYFILE>" << endl;
     return 0;
+  }
+
+  testGraphDriver(argc, argv);
+
+  return 0;
 }
--- graph.h
+++ graph.h
@@ -14,33 +14,34 @@
 */
 class Vertex
 {
+public:
+    /*
+    Will be used for reading in the vectors that are connect to the source vector
+    */
+    struct AdjVertex
+    {
     public:
-        /*
-            Will be used for reading in the vectors that are connect to the source vector
-        */
-        typedef struct AdjVertex
+        AdjVertex(const int &vertex, const double &weight)
         {
-            public:
-                AdjVertex(const int &vertex, const double &weight)
-                {
-                    vertex_ = vertex;
-                    weight_ = weight;
-                }
-                int vertex_ = 0;
-                double weight_ = 0.0;
-        };
-
-        Vertex(const int &v)
-        {
-            v_ = v;
-            // dest_ = dest;
+            vertex_ = vertex;
+            weight_ = weight;
         }
-        
-        // List of all the adjacent vertices of the current vector   
-        vector<AdjVertex> adjList;
-    private:
-        int v_ = 0;
-        // Vertex* dest_;
+        int vertex_ = 0;
+        double weight_ = 0.0;
+    };
+
+    Vertex(const int &v)
+    {
+        v_ = v;
+        // dest_ = dest;
+    }
+
+    // List of all the adjacent vertices of the current vector
+    vector<AdjVertex> adjList;
+
+private:
+    int v_ = 0;
+    // Vertex* dest_;
 };
 
 /*
@@ -48,61 +49,63 @@ class Vertex
 */
 class Graph
 {
-    public:
-        /*
+public:
+    /*
             Creates a graph given the number of vertices in it
         */
-        Graph(const int &v) : vertices{v + 1}
+    Graph(const int &v) : vertices{v + (size_t)1}
+    {
+        for (int i = 1; i < v + 1; i++)
         {
-            for (int i = 1; i < v + 1; i++)
-            {
-                vertices[i] = new Vertex(i);
-            }
+            vertices[i] = new Vertex(i);
         }
+    }
 
-        /*
+    /*
             Adds adjacent vertex(adjv) to current vector(v) list
         */
-        void addEdge(const int &v, const int &adjv, const double &weight)
-        {
-            // Creates an object to hold the adjacent vertex
-            Vertex::AdjVertex adj_Vertex(adjv, weight);
-            // Adds a destination vertex to the source vertex list
-            vertices[v]->adjList.push_back(adj_Vertex);
-            // If this were an undirected graph you would add the bottom instruction
-            // vertices[adj_Vertex]->adjList.push_back(v);
-        }
+    void addEdge(const int &v, const int &adjv, const double &weight)
+    {
+        // Creates an object to hold the adjacent vertex
+        Vertex::AdjVertex adj_Vertex(adjv, weight);
+        // Adds a destination vertex to the source vertex list
+        vertices[v]->adjList.push_back(adj_Vertex);
+        // If this were an undirected graph you would add the bottom instruction
+        // vertices[adj_Vertex]->adjList.push_back(v);
+    }
 
-        /*
+    /*
             Checks if current(src) vertex is connected with given(dest) vertex
         */
-        bool isConnected(int src, int dest)
+    void isConnected(int src, int dest)
+    {
+        // Will be used when checking if current(src) vertex is connected to other(dest) vertex
+        bool adjacent = false;
+        // size of list
+        int size = vertices[src]->adjList.size();
+
+        // Loops through the list of vertices
+        for (int i = 0; i < size; i++)
         {
-            // Will be used when checking if current(src) vertex is connected to other(dest) vertex
-            bool adjacent = false;
-            // size of list
-            int size = vertices[src]->adjList.size();
-
-            // Loops through the list of vertices
-            for (int i = 0; i < size; i++)
+            // Checks dest is in adjList
+            if (vertices[src]->adjList[i].vertex_ == dest)
             {
-                // Checks dest is in adjList
-                if (vertices[src]->adjList[i].vertex_ = dest)
-                {
-                    // Adjacent vertex found
-                    adjacent = true;
-                    // Print statement as per assignment rules
-                    std::cout << src << " " << dest << ": connected " << vertices[src]->adjList[i].weight_ << std::endl;
-                    // will return true because adjacent vertex was found
-                    return adjacent;
-                }
+                // Adjacent vertex found
+                adjacent = true;
+                // Print statement as per assignment rules
+                std::cout << src << " " << dest << ": connected " << vertices[src]->adjList[i].weight_ << std::endl;
+                break;
             }
-            // will return false because no adjacent vertex was found
-            return adjacent;
         }
+        if (adjacent == false)
+        {
+            std::cout << src << " " << dest << ": not_connected " << std::endl;
+        }
+        // will return false because no adjacent vertex was found
+    }
 
-    private:
-    vector<Vertex*> vertices;
+private:
+    vector<Vertex *> vertices;
 };
 
 #endif
