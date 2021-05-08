ed305c08bf066ca374d39c9f64996fd8f1a1f0ae
Arnoldo Solis <arnoldosolis817@gmail.com>
2021-05-08 00:23:46 -0400

    Segmentation Fault for somehwere in printing shortest path

--- FindPaths.cc
+++ FindPaths.cc
@@ -18,9 +18,81 @@ int findPathDriver(int argc, char **argv)
   //Begin your code here. Feel free to add any helper functions or classes you need,
   //as long as we only have to call this function to run the specified assignment.
   const string graph_filename(argv[1]);
-  const string starting_vertex_filename(argv[2]);
+  const int starting_vertex(atoi(argv[2]));
   
-  std::ifstream graph_filename()
+  // Initialize ifstream object
+  std::ifstream graphTxt;
+  // Open file
+  graphTxt.open(graph_filename);
+  // Checks if file is able to open or if it existss
+  if (graphTxt.fail())
+  {
+    // Prints error message
+    std::cout << "Unable to open file or file does not exist!" << std::endl;
+    // Exits
+    exit(1);
+  }
+
+  // Will be used to read in graph file
+  std::string graph_input;
+  // stores amount of vertices that is retrieved from first line of text file
+  std::string amountOfV;
+  // Gets the string number of vertices in graph
+  getline(graphTxt, amountOfV);
+  // Converter
+  std::stringstream amountOfVInt(amountOfV);
+  // This will be where the amount of vertices will be stored
+  int v = 0;
+  // Converts the string number to int and inserts into int v
+  amountOfVInt >> v;
+  // Prints amount of vertices in graph
+  // std::cout << "There are " << v << " vertices in the graph" << std::endl;
+
+  // Initialize graph object
+  Graph directedGraph(v);
+
+  while (getline(graphTxt, graph_input))
+  {
+    // Will be used to traverse source vertex
+    int vertexIterator = 1;
+    //
+    int vertex = 0;
+    int connectedVertex = 0;
+    double weight = 0.0;
+    std::stringstream graphinput(graph_input);
+    // std::cout << graph_input << std::endl;
+    // While string stream object is being read to
+    while (graphinput)
+    {
+      // Iterates from first vector that is being read (this happens after the amount of vertices that will be in graph has been read in)
+      if (vertexIterator == 1)
+      {
+        graphinput >> vertex;
+        //std::cout << "vertex: " << vertex << std::endl;
+      }
+      // Gets connected vertex from graphinput and inserts into connectedVertex
+      graphinput >> connectedVertex;
+      //std::cout << "connected vertex:" << connectedVertex << std::endl;
+      // Gets weight of connected vertex from graph input and inserts into weight
+      graphinput >> weight;
+      //std::cout << "weight of connected vertex: " << weight << std::endl;
+      // Gets connected vertex and its weight and adds it to current vertex adjacent list
+      if (graphinput)
+      {
+        directedGraph.addEdge(vertex, connectedVertex, weight);
+      }
+
+      // goes to next vertex
+      vertexIterator++;
+    }
+  }
+  graphTxt.close();
+  // std::cout << "Graph was read" << std::endl;
+
+  std::cout << "Starting vertex: " << starting_vertex << std::endl;
+
+  directedGraph.DijkstraAlgorithm(starting_vertex);
+  
 
   return 0;
 }
--- graph.h
+++ graph.h
@@ -30,18 +30,21 @@ public:
         double weight_ = 0.0;
     };
 
-    Vertex(const int &v)
+    Vertex(const int &v, const bool &known, const double &dist, Vertex *path)
     {
         v_ = v;
-        // dest_ = dest;
+        known_ = known;
+        dist_ = dist;
+        path_ = path;
     }
 
     // List of all the adjacent vertices of the current vector
     vector<AdjVertex> adjList;
-
-private:
+    // Variables
     int v_ = 0;
-    // Vertex* dest_;
+    bool known_ = false;
+    double dist_ = 0.0;
+    Vertex* path_;
 };
 
 /*
@@ -51,13 +54,13 @@ class Graph
 {
 public:
     /*
-            Creates a graph given the number of vertices in it
-        */
+        Creates a graph given the number of vertices in it
+    */
     Graph(const int &v) : vertices{v + (size_t)1}
     {
         for (int i = 1; i < v + 1; i++)
         {
-            vertices[i] = new Vertex(i);
+            vertices[i] = new Vertex(i, false, 0.0, nullptr);
         }
     }
 
@@ -104,8 +107,102 @@ public:
         // will return false because no adjacent vertex was found
     }
 
+    void DijkstraAlgorithm(const int &starting_vertex)
+    {
+        // Pseudocode for Dijkstra's Algorithm
+        // for each Vertex v
+        // {
+        //     v.dist = INFINITY;
+        //     v.known = false;
+        // }
+        // s.dist = 0;
+        // while( there is an unknown distance vertex )
+        // {
+        //     Vertex v = smallest unknown distance vertex;
+        //     v.known = true;
+        //     for each Vertex w adjacent to v
+        //     if( !w.known )
+        //     {
+        //         DistType cvw = cost of edge from v to w;
+        //         if( v.dist + cvw < w.dist )
+        //         {
+        //             // Update w
+        //             decrease( w.dist to v.dist + cvw );
+        //             w.path = v;
+        //         }
+        //     }
+        // }
+        
+        vertices[starting_vertex]->dist_ = 0;
+        BinaryHeap<Vertex*> que;
+        que.insert(vertices[starting_vertex]);
+        
+        while (!(que.isEmpty()))
+        {
+            Vertex* v = que.findMin();
+            que.deleteMin();
+            v->known_ = true;
+
+            int size = v->adjList.size();
+            for (int i = 0; i < size; i++)
+            {
+                int a_vertex = v->adjList[i].vertex_;
+                Vertex* unknown_vertex = vertices[a_vertex];
+
+                if (!unknown_vertex->known_)
+                {
+                    // Thought I name the variable cost it is a representation of 
+                    // the weight of the edge
+                    double cost = v->adjList[i].weight_;
+                    if (v->dist_ + cost < unknown_vertex->dist_)
+                    {
+                        unknown_vertex->dist_ = v->dist_ + cost;
+                        unknown_vertex->path_ = v;
+                    }
+                    que.insert(unknown_vertex);
+                }
+            }
+        }
+
+        int size = vertices.size();
+        // Print shortest path
+        std::cout << "HERE: Printing shortest path" << std::endl;
+        for (int i = 0; i < size; i++)
+        {
+            int counter = 1;
+            std::cout << counter << std::endl;   
+            // std::cout << vertices[i]->v_ << ": ";
+            //double shortest_path = vertices[i]->dist_;
+
+            // printPath(vertices[i]);
+            // if(shortest_path > 0)
+            // {
+            //     std::cout << "(Cost: " << shortest_path << ")" << std::endl;
+            // }
+            // else
+            // {
+            //     std::cout << "not_possible" << std::endl;
+            // }
+            counter++;
+        }
+    }
+
+    /*
+        This function was taken from the book
+        It prints the shortest path to v after dijkstra has run.
+        Assume that the path exists.
+    */
+    void printPath(Vertex* v)
+    {
+        if (v->path_ != nullptr)
+        {
+            printPath(v->path_);
+            std::cout << " to ";
+        }
+        std::cout << v->v_ << " ";
+    }
 private:
-    vector<Vertex *> vertices;
+    vector<Vertex*> vertices;
 };
 
 #endif
